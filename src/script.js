5c842b745e1599ae67d8e0bff082fbbf81af6b63
demib999 <db67mail@gmail.com>
2025-04-14 23:43:19 +1000

    Added basic form handling and added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
+function generateRhyme(event) {
+  event.preventDefault();
 
+  let rhymeElement = document.querySelector("#rhyme");
+
+  new Typewriter("#rhyme", {
+    strings: "Tell me not, in mournful numbers",
+    autoStart: true,
+    cursor: null,
+    delay: 40,
+  });
+}
+
+let rhymeFormElement = document.querySelector("#rhyme-generator-form");
+rhymeFormElement.addEventListener("submit", generateRhyme);
